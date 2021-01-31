cf2640e55f925b5f79f87e9257631cb09df74fbb
LAZARO <lazarolimasilva2@gmail.com>
2021-01-31 14:51:20 -0300

    erro for insert at DOM

--- scripts.js
+++ scripts.js
@@ -6,3 +6,69 @@ const Modal = {
         document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
+
+const Transactions = [
+    {
+        id:1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        id:2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+    {
+        id:3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    }
+]
+
+
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses(){
+        //somas as saidas
+    },
+    total(){
+        //entradas - saidas
+    }
+}
+
+
+const DOM =  {
+
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        console.log(tr)
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction){
+
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>    
+        `
+
+        return html
+    }
+}
+
+DOM.addTransaction(Transactions[0]) 
