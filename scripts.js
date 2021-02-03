a557767662423773237a9c804e0242da5e2d293c
Lazaro1 <lazarolimasilva2@gmail.com>
2021-02-03 16:55:35 -0300

    amount

--- scripts.js
+++ scripts.js
@@ -51,16 +51,17 @@ const DOM =  {
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
-        console.log(tr)
         DOM.transactionsContainer.appendChild(tr)
     },
 
     innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="expense">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
@@ -71,4 +72,23 @@ const DOM =  {
     }
 }
 
-DOM.addTransaction(Transactions[0]) 
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style:"currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+Transactions.forEach((transaction) => {
+    DOM.addTransaction(transaction) 
+})
