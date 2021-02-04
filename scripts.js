10904699f88d118c6c1d02c2a7f5114849abaddc
LAZARO <lazarolimasilva2@gmail.com>
2021-02-03 21:57:47 -0300

    geting values with form

--- scripts.js
+++ scripts.js
@@ -7,51 +7,81 @@ const Modal = {
     }
 }
 
-const Transactions = [
-    {
-        id:1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id:2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id:3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    }
-]
-
-
-
 const Transaction = {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Site',
+            amount: 200000,
+            date: '23/01/2021'
+        }
+    ],
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index){
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
     incomes() {
-        //somar as entradas
+        let income = 0;
+    
+        Transaction.all.forEach((transaction) => {
+            
+            if (transaction.amount > 0 ) {
+
+                income += transaction.amount;
+            }
+        })
+
+        return income;
     },
     expenses(){
-        //somas as saidas
+        let expense = 0;
+
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            } 
+        })
+        return expense
     },
     total(){
-        //entradas - saidas
+
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
 
 const DOM =  {
 
-    transactionsContainer: document.querySelector('#data-table tbody'),
+    TransactionsContainer: document.querySelector('#data-table tbody'),
     
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
-        DOM.transactionsContainer.appendChild(tr)
+        DOM.TransactionsContainer.appendChild(tr)
     },
 
     innerHTMLTransaction(transaction){
@@ -69,6 +99,17 @@ const DOM =  {
         `
 
         return html
+    },
+
+    updateBalance(){
+        document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions(){
+        DOM.TransactionsContainer.innerHTML = ""
+        console.log('passou')
     }
 }
 
@@ -89,6 +130,30 @@ const Utils = {
     }
 }
 
-Transactions.forEach((transaction) => {
-    DOM.addTransaction(transaction) 
+const Form = {
+    
+}
+
+const App = {
+    init() {
+
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction) 
+        })
+        
+        DOM.updateBalance()
+    },
+    reload(){
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+
+Transaction.add({
+    description: 'Cheguei',
+    amount: 200,
+    date: '23/01/2021'
 })
